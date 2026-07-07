12087e3313b6cca2f56f6aff4c73d758c85f9a59
st0ckk <anakin.salazar07@gmail.com>
2026-07-06 19:50:08 -0600

    s

---
+++ SyncroBE-Gabriel/Seed/UserSeeder.cs
@@ -0,0 +1,44 @@
+﻿using SyncroBE.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace SyncroBE.API.Seed
+{
+    public static class UserSeeder
+    {
+        public static async Task SeedAsync(IServiceProvider services)
+        {
+            using var scope = services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<SyncroDbContext>();
+
+            const string email = "super@syncro.local";
+            const string password = "Admin123*";
+
+            var user = await context.Users.FirstOrDefaultAsync(u => u.UserEmail == email);
+
+            var hash = BCrypt.Net.BCrypt.HashPassword(password);
+
+            if (user == null)
+            {
+                context.Users.Add(new User
+                {
+                    UserName = "Super",
+                    UserLastname = "Usuario",
+                    UserEmail = email,
+                    UserRole = "SuperUsuario",
+                    PasswordHash = hash,
+                    IsActive = true,
+                    CreatedAt = DateTime.UtcNow
+                });
+            }
+            else
+            {
+                // 🔥 fuerza actualización del hash inválido
+                user.PasswordHash = hash;
+                user.UserRole = "SuperUsuario";
+                user.IsActive = true;
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
